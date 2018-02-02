a96fc84029841e41f8ef9a61513969c7771fdee4
muji <muji@tmpfs.org>
2018-02-02 18:28:02 +0800

    Use custom fonts on android.

--- app/screens/ScreenRenderer.js
+++ app/screens/ScreenRenderer.js
@@ -4,12 +4,16 @@ import ScrollPane from '../../src/ScrollPane'
 import List from '../../src/List'
 import Panel from '../../src/Panel'
 
+// NOTE: we add 40 to the padding to fix a bug
+// NOTE: on android where it collapses the margin/padding
+// NOTE: this may result in too much space on iOS
+
 class ScreenRenderer extends Component {
   render() {
     const {list} = this
     return (
       <ScrollPane padding={20}>
-        <List space={20}>
+        <List space={20} padding={{bottom: 40}}>
         {
           list.map((item, i) => {
             return (
