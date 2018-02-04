bd83fa44d35044fb26d82a46ba097c25213fb04b
muji <muji@tmpfs.org>
2018-02-05 02:14:09 +0800

    Update for reactivity.

--- app/screens/NoticeScreen.js
+++ app/screens/NoticeScreen.js
@@ -3,9 +3,31 @@ import React, {Component} from 'react'
 import ScreenRenderer from './ScreenRenderer'
 import BackNavigationHeader from '../../src/BackNavigationHeader'
 
+import Button from '../../src/TouchButton'
 import Notice from '../../src/Notice'
+import List from '../../src/List'
 
 const copy = `Minimal, idiomatic style management for React Native.`
+
+class NoticeStateExample extends Component {
+  state = {
+    error: true
+  }
+
+  render () {
+    const toggle = () => {
+      this.setState({error: !this.state.error})
+    }
+    return (
+      <List space={10}>
+        <Notice error={this.state.error}>
+          {copy}
+        </Notice>
+        <Button onPress={toggle}>Toggle Error</Button>
+      </List>
+    )
+  }
+}
 
 class NoticeScreen extends ScreenRenderer {
   static title = 'Notice'
@@ -24,10 +46,7 @@ class NoticeScreen extends ScreenRenderer {
     {
       label: 'Error',
       component: (
-        <Notice error>
-          {copy}
-        </Notice>
-
+        <NoticeStateExample />
       )
     }
   ]
