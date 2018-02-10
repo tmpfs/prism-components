6a35c2930a873278f6978e47a706e9c9f476d32d
muji <muji@tmpfs.org>
2018-02-10 10:45:44 +0800

    Support screen argument for BackNavigationHeader.

--- src/BackNavigationHeader.js
+++ src/BackNavigationHeader.js
@@ -4,12 +4,15 @@ import BackNavigation from './BackNavigation'
 
 // Small utility to render a styled back navigation
 // in StackNavigator, TabNavigator etc
-const BackNavigationHeader = (title) => {
+const BackNavigationHeader = (title, screen) => {
   return (props) => {
     return (
       <BackNavigation
         onPress={
           () => {
+            if (screen) {
+              return props.navigation.navigate(screen)
+            }
             props.navigation.goBack()
           }
         }>
