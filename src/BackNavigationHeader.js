558834ac0881fb000807bf4980f82043a019a0c9
muji <muji@tmpfs.org>
2018-03-26 10:14:19 +0800

    Call Prism() earlier (back navigation).

--- src/BackNavigationHeader.js
+++ src/BackNavigationHeader.js
@@ -6,79 +6,77 @@ import namespace from './namespace'
 import {View} from 'react-native-prism-primitives'
 import BackNavigation from './BackNavigation'
 
+class BackNavigationHeader extends Component {
+  static styleName = 'BackNavigationHeader'
+
+  //static styleOptions = {
+    //flat: true
+  //}
+
+  render () {
+    const {style, options, screen} = this.props
+    const {title} = options
+
+    //console.log(style)
+    //console.log(title)
+
+    let {
+      onPress,
+      dismissKeyboard,
+      headerRight
+    } = options
+
+    if (dismissKeyboard === undefined) {
+      dismissKeyboard = true
+    }
+
+    if (!onPress) {
+      onPress=
+        () => {
+          if (dismissKeyboard) {
+            Keyboard.dismiss()
+          }
+          if (screen) {
+            return this.props.navigation.navigate(screen)
+          }
+          this.props.navigation.pop()
+        }
+    } else {
+      const parentOnPress = onPress
+      onPress = () => {
+        if (dismissKeyboard) {
+          Keyboard.dismiss()
+        }
+        parentOnPress(this.props)
+      }
+    }
+
+    const headerLeft = options.headerLeft || (
+      <BackNavigation onPress={onPress}>
+        {title}
+      </BackNavigation>
+    )
+
+    // NOTE: must reset position to relative
+    return (
+      <View style={[style, {position: 'relative'}]}>
+        {headerLeft}
+        {headerRight}
+      </View>
+    )
+  }
+}
+
+const Header = Prism(BackNavigationHeader, {namespace})
+
 // Small utility to render a styled back navigation
 // in StackNavigator, TabNavigator etc
-const BackNavigationHeader = (screen, options = {title: 'Back'}) => {
-  const {title} = options
+
+// NOTE: must export a function for `react-navigation`
+export default (screen, options = {title: 'Back'}) => {
   if (typeof(screen) === 'function') {
     options.onPress = screen
     screen = null
   }
-
-  class BackNavigationHeader extends Component {
-    static styleName = 'BackNavigationHeader'
-
-    //static styleOptions = {
-      //flat: true
-    //}
-
-    render () {
-      const {props} = this
-      const {style} = props
-
-      //console.log(style)
-      //console.log(title)
-
-      let {
-        onPress,
-        dismissKeyboard,
-        headerRight
-      } = options
-
-      if (dismissKeyboard === undefined) {
-        dismissKeyboard = true
-      }
-
-      if (!onPress) {
-        onPress=
-          () => {
-            if (dismissKeyboard) {
-              Keyboard.dismiss()
-            }
-            if (screen) {
-              return props.navigation.navigate(screen)
-            }
-            props.navigation.pop()
-          }
-      } else {
-        const parentOnPress = onPress
-        onPress = () => {
-          if (dismissKeyboard) {
-            Keyboard.dismiss()
-          }
-          parentOnPress(props)
-        }
-      }
-
-      const headerLeft = options.headerLeft || (
-        <BackNavigation onPress={onPress}>
-          {title}
-        </BackNavigation>
-      )
-
-      // NOTE: must reset position to relative
-      return (
-        <View style={[style, {position: 'relative'}]}>
-          {headerLeft}
-          {headerRight}
-        </View>
-      )
-    }
-  }
-
-  const Header = Prism(BackNavigationHeader, {namespace})
-  // NOTE: must export a function for `react-navigation`
-  return (props) => <Header {...props} />
+  return (props) => <Header {...props} screen={screen} options={options} />
 }
-
-export default BackNavigationHeader
